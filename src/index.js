d7ec2c2dc8dc245b835a7d4433123c4a4e6a1e58
Santiago Lorenzo <santylore4@gmail.com>
2019-05-01 19:56:43 -0300

    App component passed to Hooks

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import rootReducer from './Config/rootReducer'
-import App from './App';
+// import rootReducer from './Config/rootReducer'
+import App from './components/app/App';
 import './index.css';
 
 const store = createStore(
-  rootReducer,
+  // rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
