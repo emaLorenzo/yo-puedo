58a588a6259c25646bb4aed09a243db0a8f85f25
Ema Lorenzo <aemalorenzo@gmail.com>
2019-05-13 16:47:51 -0300

    Auth component

---
+++ src/components/Authentication/Authentication.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+
+const Authentication = () => {
+  return <Paper elevation={2}>Auth</Paper>;
+};
+
+export default Authentication;
